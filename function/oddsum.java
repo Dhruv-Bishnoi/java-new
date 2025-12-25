378663cd5253953549a5f32a06e5c6f9dbf001f2
dhruv bishnoi <bishnoishab502@gmail.com>
2025-12-25 19:33:40 +0530

    odd

--- function/oddsum.java
+++ function/oddsum.java
@@ -2,7 +2,16 @@ package function;
 //Write a function to print the sum of all odd numbers from 1 to n.
 public class oddsum {
 
+    // public static sumodd(){}
     public static void main(String[] args) {
+        int n = 30;
+        int sum = 0;
+        for(int i = 1 ; i<=n;i++ ){
+            if (i%2!=0) {
+                sum = sum+i;    
+            }
+        }
+        System.out.println(sum);
         
     }
     
