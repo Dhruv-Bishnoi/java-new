e4eb38f941b6179dc4f165028feaafdf3b224d79
dhruv bishnoi <bishnoishab502@gmail.com>
2025-12-26 20:52:28 +0530

    arry

---
+++ function/arry.java
@@ -0,0 +1,16 @@
+package function;
+
+public class arry {
+    public static void main(String[] args) {
+        int[] marks = new int[4];
+        marks[0]= 13;
+        marks[1]=32;
+        marks[3]=55;
+
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[3]);
+        
+    }
+}
