386d1e0493ed082ee87896aaa83c09ee2dfcbe42
dhruv bishnoi <bishnoishab502@gmail.com>
2026-01-01 21:40:45 +0530

    Update stringg.java

--- stringg.java
+++ stringg.java
@@ -13,6 +13,19 @@ public class stringg {
         String name3 = " hello my name is dhruv bishnoi ";
         String namesub = name3.substring(7,11);
         System.out.println(namesub);
+        System.out.println(name1.charAt(0));
 
+
+        StringBuilder sb = new StringBuilder("dhruv bishnoi");
+
+        System.out.println(sb.charAt(0));
+        sb.setCharAt(0, 'b');
+        System.out.println(sb);
+        sb.insert(0, "d");
+        System.out.println(sb);
+        sb.delete(0, 2);
+
+
+        sb.append("h")
     }
 }
