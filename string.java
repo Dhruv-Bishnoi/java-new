ce76cc7324064c0231228d1769b2cddeb05e4cb5
dhruv bishnoi <bishnoishab502@gmail.com>
2026-01-01 21:07:44 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class string {
+    public static void main(String[] args) {
+        
+        String name1 = "hello";
+        String name2 = "hello";
+        System.out.println(name1);
+        if (name1.compareTo(name2)==) {
+            System.out.println("is equal");
+            
+        }
+
+    }
+}
