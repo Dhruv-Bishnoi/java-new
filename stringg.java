f007565281de7965f3fba0c503eec6c34ca507fc
dhruv bishnoi <bishnoishab502@gmail.com>
2026-01-01 21:24:30 +0530

    string

--- stringg.java
+++ stringg.java
@@ -1,15 +1,18 @@
-import java.util.*;
 
-public class string {
+public class stringg {
     public static void main(String[] args) {
         
         String name1 = "hello";
         String name2 = "hello";
         System.out.println(name1);
-        if (name1.compareTo(name2)==) {
+        if (name1.compareTo(name2)==0) {
             System.out.println("is equal");
             
         }
+
+        String name3 = " hello my name is dhruv bishnoi ";
+        String namesub = name3.substring(7,11);
+        System.out.println(namesub);
 
     }
 }
