ce9a5197d629d2fa8f9970dfc3ec19847d5453aa
dhruv bishnoi <bishnoishab502@gmail.com>
2026-01-05 12:30:59 +0530

    Add BubbleShort.java with bubble sort implementation
    Introduces a new Java class BubbleShort that implements the bubble sort algorithm to sort an integer array and print the sorted elements.

---
+++ BubbleShort.java
@@ -0,0 +1,25 @@
+import function.arry;
+
+public class BubbleShort {
+    public static void main(String[] args) {
+        int arr[] = {4,6,1,9,7,5,3,2};
+        int n = arr.length;
+
+
+        for(int i=0; i<n ; i++){
+            for(int j = 0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+
+                }
+            }
+        }
+
+        for(int i = 0; i<n;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
